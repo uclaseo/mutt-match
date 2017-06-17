b09114051d36e0d2ee050b2fcc41be4009646e6d
Adewale Balogun <abalogun93@gmail.com>
2017-06-17 15:16:02 -0700

    [Delete] -package-lock.json

---
+++ public/states/questionnaire/QuestCtrl.js
@@ -0,0 +1,26 @@
+angular.module('mutt-match', /*['ngMaterial']*/)
+
+.controller('QuestCtrl', [function($mdDialog) {
+  /*this.showPrompt = function(ev) {
+    var confirm = $mdDialog.prompt()
+      .title('Are you an active person?')
+      .targetEvent(ev)
+      .ok('Pretty active')
+      .cancel('Not so much');
+
+      $mdDialog.show(confirm).then(() => {
+        this.status = 'Active is good for dogs!';
+      }, function() {
+        this.status = 'No sweat for you!';
+      });
+  };*/
+}]);
+  // .config(function($mdThemingProvider) {
+
+  //   // Configure a dark theme with primary foreground yellow
+
+  //   $mdThemingProvider.theme('docs-dark', 'default')
+  //     .primaryPalette('yellow')
+  //     .dark();
+
+  // });
---
+++ public/states/questionnaire/quest.js
@@ -0,0 +1,8 @@
+angular.module('mutt-match')
+
+.component('questionnaire', {
+  bindings: {},
+  controller: 'QuestCtrl',
+  restrict: 'E',
+  templateUrl: './states/questionnaire/quest.html'
+});
