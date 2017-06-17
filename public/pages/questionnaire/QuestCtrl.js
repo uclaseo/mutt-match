b206cbac7a753ac7b35eca68fcd86f2b2878b652
Adewale Balogun <abalogun93@gmail.com>
2017-06-16 20:06:56 -0700

    Connects to server, still needs work on display

---
+++ public/pages/questionnaire/QuestCtrl.js
@@ -0,0 +1,21 @@
+angular.module('mutt-match')
+
+.controller('QuestCtrl', [function() {
+  // this.user = {
+  //   title: 'Developer',
+  //   email: '',
+  //   firstName: '',
+  //   lastName: '',
+  //   city: '',
+  //   postalCode: ''
+  // };
+}]);
+  // .config(function($mdThemingProvider) {
+
+  //   // Configure a dark theme with primary foreground yellow
+
+  //   $mdThemingProvider.theme('docs-dark', 'default')
+  //     .primaryPalette('yellow')
+  //     .dark();
+
+  // });
