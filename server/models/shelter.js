203503731fb37eaaccabf2a4a3ea80cbdc501186
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-06-17 08:58:27 -0700

    (ADD) Gets  build scripts to run correctly

--- server/models/shelter.js
+++ server/models/shelter.js
@@ -7,7 +7,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        models.Shelter.hasMany(models.Dog { targetKey: 'shelter' });
+        models.Shelter.hasMany(models.Dog, { targetKey: 'shelter' });
       }
     }
   });
--- server/seeders/20170617024154-shelters.js
+++ server/seeders/20170617024154-shelters.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Users', sheltersData);
+    return queryInterface.bulkInsert('Shelters', sheltersData);
   },
 
   down: function (queryInterface, Sequelize) {
