c78abc4f26245b786ca3f6473315c324cc1d3676
tru36rit <nathan.dunn.aml@gmail.com>
2017-06-24 10:19:46 -0700

    (EDIT) Fix conflicts in index.js

--- public/index.js
+++ public/index.js
@@ -2,7 +2,6 @@ angular.module('mutt-match', ['ui.router', 'auth0.auth0'])
 
 .config(['$stateProvider', '$urlServiceProvider', 'angularAuth0Provider', '$locationProvider', function($stateProvider, $urlServiceProvider, angularAuth0Provider, $locationProvider) {
 
-<<<<<<< HEAD
   $urlServiceProvider.rules.otherwise({ state: 'home' });
 
   $stateProvider
@@ -33,20 +32,6 @@ angular.module('mutt-match', ['ui.router', 'auth0.auth0'])
       component: 'contact'
     })
 
-=======
-  $stateProvider.state('home', {
-    url: '/',
-    component: 'home',
-    resolve: {
-      login: (auth) => auth.login
-    }
-  })
-  .state('callback', {
-    url: '/callback',
-    component: 'callback'
-  });
->>>>>>> cab0ec63aa637200a4f9c2f4bed4c7788e9b0789
-
   angularAuth0Provider.init({
     clientID: 'R6TjzEfP3EdjIfAAcLMOxnsFYzYua1nY',
     domain: 'max-hoffman.auth0.com',
@@ -61,10 +46,6 @@ angular.module('mutt-match', ['ui.router', 'auth0.auth0'])
     enabled: true,
     requireBase: false
   });
-<<<<<<< HEAD
 
 }]);
-=======
-  $urlServiceProvider.rules.otherwise({ state: 'home' });
-}]);
->>>>>>> cab0ec63aa637200a4f9c2f4bed4c7788e9b0789
+
