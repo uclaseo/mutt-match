4f24822e4c9b60b204395dc1befe0d2c2efc125d
tru36rit <nathan.dunn.aml@gmail.com>
2017-06-24 10:46:02 -0700

    (EDIT) fix merge conflicts

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const express = require('express'),
 
 const app = express();
 
-<<<<<<< HEAD
 app
   .use(parser.json())
   .use(parser.urlencoded({ extended: true }))
@@ -16,16 +15,6 @@ app
   .use(express.static(path.join(__dirname, '../public')))
   .use(express.static(path.join(__dirname, '../node_modules')))
   .use('/', require('./routes'));
-=======
-app.use(parser.json())
-   .use(parser.urlencoded({ extended: true }))
-   .use(morgan(':method :url :status :res[content-length] - :response-time ms'))
-   .use(require('cookie-parser')())
-   .use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }))
-   .use(express.static(path.join(__dirname, '../public')))
-   .use(express.static(path.join(__dirname, '../node_modules')))
-   .use('/', require('./routes'));
->>>>>>> cab0ec63aa637200a4f9c2f4bed4c7788e9b0789
 
 db.sequelize
   .authenticate()
@@ -37,7 +26,4 @@ db.sequelize
   .catch(function(error) {
     console.log("Error creating connection:", error);
   });
-<<<<<<< HEAD
-=======
       
->>>>>>> cab0ec63aa637200a4f9c2f4bed4c7788e9b0789
