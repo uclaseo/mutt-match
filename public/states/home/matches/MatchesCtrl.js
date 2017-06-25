a3dee7b29fe74b68516789012f4711470e515dd6
tru36rit <nathan.dunn.aml@gmail.com>
2017-06-24 23:53:15 -0700

    (EDIT) Looking at matches renderings

--- public/states/home/matches/MatchesCtrl.js
+++ public/states/home/matches/MatchesCtrl.js
@@ -4,8 +4,11 @@ angular.module('mutt-match')
 
   this.dogs = [];
 
+  this.getRandomArbitrary = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
   matchesService.fetchMatches()
     .then(matches => this.dogs = matches)
+    .then(matches => matches.forEach(match => match.score = this.getRandomArbitrary(60, 99)))
     .then(matches => $log.log('!!!', this.dogs))
     .catch(err => console.error(err));
 
