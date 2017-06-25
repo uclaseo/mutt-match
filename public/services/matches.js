a9b3429485ace9e486eee8b1af0dcff88c70a9e4
tru36rit <nathan.dunn.aml@gmail.com>
2017-06-24 23:09:45 -0700

    (ADD) Adds service to get dog object matches from db and renders to the page

--- public/services/matches.js
+++ public/services/matches.js
@@ -3,10 +3,7 @@ angular.module('mutt-match')
 .service('matchesService', ['$http', '$log', function($http, $log) {
   let id = 8;
 
-  $log.log('*** matchesService firing!!! ***');
-
   let matches = [];
-  let dogs = [];
 
   this.fetchMatches = function() {
     return $http.get(`/users/${id}/matches`) //hard-coded id for now
@@ -17,13 +14,5 @@ angular.module('mutt-match')
       .catch(err => $log.log('err', err));
   };
 
-  this.fetchDogs = function() {
-    return $http.get(`/dogs`)
-      .then(resp => {
-        dogs = resp.data;
-        return dogs;
-      })
-      .catch(err => $log.log('err', err));
-  }
 
 }]);
--- server/routes/matches/matches.js
+++ server/routes/matches/matches.js
@@ -4,7 +4,10 @@ module.exports = {
 
   findAllMatchesCtrl: function(req, res) {
     let id = req.params.id;
+    let dogObjs = [];
+
     db.Match.findAll({
+        attributes: ['dog', 'score'],
         where: {
           user: id,
           score: {
@@ -12,8 +15,25 @@ module.exports = {
           }
         }
       })
+      .then(matches => {
+        dogObjs = matches;
+        let promises = dogObjs.map(dogObj => {
+          let score = dogObj.dataValues.score;
+
+          return db.Dog.findOne({
+            where: {
+              id: dogObj.dataValues.dog
+            }
+          });
+        });
+        return Promise.all(promises);
+      })
+      .then(results => {
+        return results;
+      })
       .then(results => res.status(200).json(results))
       .catch(error => res.sendStatus(500, error));
+
   }
 
 };
