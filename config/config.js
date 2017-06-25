ba8291231640ec2ed209b62b0d905a93797acf0b
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-06-25 13:27:23 -0700

    (EDIT) connects to extrenal postgresdb, will likely change

--- config/config.js
+++ config/config.js
@@ -1,10 +1,16 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "mutt_match",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
+    // "username": "root",
+    // "password": null,
+    // "database": "mutt_match",
+    // "host": "127.0.0.1",
+    // "dialect": "mysql",
+    "username": "zsuvsyix",
+    "password": 'rVk8_mEu3qGGjx98r1JWIsmX9i2oRlVo',
+    "database": "zsuvsyix",
+    "host": "pellefant.db.elephantsql.com",
+    "port": 5432,
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
@@ -14,11 +20,16 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": "password",
-    "database": "mutt_match",
-    "host": process.env.DB_PORT_3306_TCP_ADDR,
-    "dialect": "mysql",
-    "socketPath": "/var/run/mysqld/mysqld.sock"
+    // "username": "root",
+    // "password": "password",
+    // "database": "mutt_match",
+    // "host": process.env.DB_PORT_3306_TCP_ADDR,
+    // "socketPath": "/var/run/mysqld/mysqld.sock"
+    "username": "zsuvsyix",
+    "password": 'rVk8_mEu3qGGjx98r1JWIsmX9i2oRlVo',
+    "database": "zsuvsyix",
+    "host": "pellefant.db.elephantsql.com",
+    "port": 5432,
+    "dialect": "postgres"
   }
 };
--- server/routes/users/users.js
+++ server/routes/users/users.js
@@ -4,26 +4,26 @@ module.exports = {
   findAllUsersCtrl: function(req, res) {
     db.User.findAll()
       .then(user => res.send(user))
-      .catch(error => res.sendStatus(500, error))
+      .catch(error => res.send(error))
   },
   findOneUserCtrl: function(req, res) {
     db.User.findOne({ where: {id: req.params.id }})
       .then(user => res.send(user))
-      .catch(error => res.sendStatus(500, error));
+      .catch(error => res.send(error));
   },
   findOrCreateUserCtrl: function(req, res) {
     db.User.findOrCreate({ where: req.body })
       .then(user => res.send(user))
-      .catch(error => res.sendStatus(500, error));
+      .catch(error => res.send(error));
   },
   updateUserCtrl: function(req,res) {
     db.User.update(req.body, { where: {id: req.params.id }})
       .then(user => res.sendStatus(201, user))
-      .catch(error => res.sendStatus(500, error));
+      .catch(error => res.send(error));
   },
   deleteUserCtrl: function(req, res) {
     db.User.destroy({ where: {id: req.params.id }})
       .then(user => res.sendStatus(200, user))
-      .catch(error => res.sendStatus(500, error));
+      .catch(error => res.send(error));
   }
 }
