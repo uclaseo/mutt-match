57c83b687509208f1906eca1643a3d7192594093
Adewale Balogun <abalogun93@gmail.com>
2017-06-25 21:17:56 -0700

    (ADD) adds checkboxes instead of text box for some questions

--- public/states/questionnaire/QuestionnaireCtrl.js
+++ public/states/questionnaire/QuestionnaireCtrl.js
@@ -1,18 +1,35 @@
 angular.module('mutt-match')
 
- .controller('QuestionnaireCtrl', ['questionnaireService','userService', '$log', function(questionnaireService, userService, $log) {
+ .controller('QuestionnaireCtrl', ['questionnaireService','userService', '$log', '$state', function(questionnaireService, userService, $log, $state) {
    this.questionnaireData = {
      active: '',
      grooming: '',
      dogSize: '',
-     noise: '',
-     experience: ''
+     noise: {
+       yes: true,
+       no: false
+     },
+     experience: {
+       yes: true,
+       no: false
+     }
    };
 
   $log.log('questionnaireService in QuestionnaireCtrl', questionnaireService.update(userService.get(this.userData)));
-   this.onClick = () => {
-     $log.log('inside click function QuestionnaireCtrl');
-     questionnaireService.update(userService.get(), this.userData)
-   }
+
+  this.submit = () => {
+    let user = localStorage.getItem('profile');
+    $log.log('inside submit of questionnaireCtrl', questionnaireService);
+    questionnaireService.update(user, this.userData)
+      .then(user => {
+        localStorage.setItem('profile', user.data);
+        $state.go(MatchesCtrl.matches);
+      })
+      .catch(error => console.error(error));
+  }
+  //  this.onClick = () => {
+  //    $log.log('inside click function QuestionnaireCtrl');
+  //    questionnaireService.update(userService.get(), this.userData)
+  //  }
 
  }]);
