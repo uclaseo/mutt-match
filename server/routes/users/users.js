3f6a1b15f5dee29039d4108c1c27ea9d3acc783a
tru36rit <nathan.dunn.aml@gmail.com>
2017-06-26 09:11:12 -0700

    (ADD) Adds default dog pic

--- server/routes/users/users.js
+++ server/routes/users/users.js
@@ -7,22 +7,25 @@ module.exports = {
       .catch(error => res.sendStatus(500, error))
   },
   findOneUserCtrl: function(req, res) {
-    db.User.findOne({ where: {id: req.params.id }})
+    db.User.findOne({ where: { id: req.params.id } })
       .then(user => res.send(user))
       .catch(error => res.sendStatus(500, error));
   },
   findOrCreateUserCtrl: function(req, res) {
     db.User.findOrCreate({ where: req.body })
       .then(user => res.send(user))
-      .catch(error => res.sendStatus(500, error));
+      .catch(error => {
+        console.log('findOrCreateUserCtrl ERROR', error);
+        res.send(error);
+      });
   },
-  updateUserCtrl: function(req,res) {
-    db.User.update(req.body, { where: {id: req.params.id }})
+  updateUserCtrl: function(req, res) {
+    db.User.update(req.body, { where: { id: req.params.id } })
       .then(user => res.sendStatus(201, user))
       .catch(error => res.sendStatus(500, error));
   },
   deleteUserCtrl: function(req, res) {
-    db.User.destroy({ where: {id: req.params.id }})
+    db.User.destroy({ where: { id: req.params.id } })
       .then(user => res.sendStatus(200, user))
       .catch(error => res.sendStatus(500, error));
   }
