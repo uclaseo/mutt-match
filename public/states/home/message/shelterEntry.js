a9bc204a13f9fea9e20f712a2760a4fbc02146f1
Inseok Seo <inseok.ucla@gmail.com>
2017-06-30 16:24:11 -0700

    add shelter text

--- public/states/home/message/shelterEntry.js
+++ public/states/home/message/shelterEntry.js
@@ -1,7 +1,43 @@
 angular.module('mutt-match')
-.controller('shelterEntryCtrl', function() {
+.controller('shelterEntryCtrl', ['messageService', function(messageService) {
+  const vm = this;
+  vm.isTrue = false;
+  vm.showText = showText;
+  vm.sendMessage = sendMessage;
 
-})
+
+  function showText() {
+    vm.isTrue = !vm.isTrue;
+  }
+  vm.openDialog = function($event) {
+    $mdDialog.show({
+      controller: DialogCtrl,
+      controllerAs: 'ctrl',
+      templateUrl: 'dialog.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: $event,
+      clickOutsideToClose:true
+    });
+  };
+
+
+  function sendMessage(text, to) {
+    console.log('send', text);
+    console.log('to', to);
+    vm.text = '';
+    messageService.sendMessage(text, to)
+    .then((response) => {
+      console.log('messageEntry sendMessage success', response);
+    })
+    .catch((error) => {
+      console.log('messageEntry sendMEssage fail', error);
+    });
+  };
+
+
+
+
+}])
 .directive('shelterEntry', function() {
   return {
     scope: {
