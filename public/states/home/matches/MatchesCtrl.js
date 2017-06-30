b45485a4a12e67b1a3f2f9659927e554513bb6c3
Jason Shao <jason@Jasons-MacBook-Pro.local>
2017-06-29 22:32:06 -0700

    functional questionnaire

--- public/states/home/matches/MatchesCtrl.js
+++ public/states/home/matches/MatchesCtrl.js
@@ -2,14 +2,12 @@ angular.module('mutt-match')
 
 .controller('MatchesCtrl', ['$log', 'matchesService', 'imagesService', 'store', function($log, matchesService, imagesService, store) {
 
-  this.dogs = [];
-
   var id = store.get('profile').userInfo.data.id
 
   matchesService.fetchMatches(id)
   .then((resp) => {
-    var fiveDogMatchesArray = resp.data.results[0].dogs;
-    console.log(fiveDogMatchesArray)
+    this.dogs = resp.data.results[0].dogs;
+    console.log(this.dogs)
   })
 
 }]);
